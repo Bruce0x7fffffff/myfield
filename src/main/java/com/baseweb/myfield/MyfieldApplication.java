3fa71acf18028d4dee8845c7ffbe8734223031de
zhouying635278 <30320124+zhouying635278@users.noreply.github.com>
2018-10-05 21:40:58 +0800

    first commit init project

---
+++ src/main/java/com/baseweb/myfield/MyfieldApplication.java
@@ -0,0 +1,12 @@
+package com.baseweb.myfield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfieldApplication.class, args);
+	}
+}
